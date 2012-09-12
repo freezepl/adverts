3eb59838fdc0aa49d06f8ef34b6fc75e195f202a
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2012-09-13 01:35:19 +0200

    dealing with categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   # check_authorization :unless => :devise_controller?
+
   rescue_from CanCan::AccessDenied do |exception|
-	redirect_to root_url, :alert => exception.message
+    redirect_to root_url, :alert => exception.message
+  end
+
+  before_filter :categories
+
+  def categories
+    @categories = Category.all
   end
 end
--- app/helpers/adverts_helper.rb
+++ app/helpers/adverts_helper.rb
@@ -1,2 +1,5 @@
 module AdvertsHelper
+  def category_list
+    Category.find(:all, :order => "name").collect {|c| [c.name, c.id]}
+  end
 end
