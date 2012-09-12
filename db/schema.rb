48290e520bdae2232f2028858e35f2ea97d7dc60
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2012-09-12 21:36:16 +0200

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120909112449) do
+ActiveRecord::Schema.define(:version => 20120912192150) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(:version => 20120909112449) do
     t.integer  "phone"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "parent_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "roles", :force => true do |t|
