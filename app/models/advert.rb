e45067f2714c5e9e49a16e4b3c9e2ba46e35fa03
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2012-09-12 21:38:56 +0200

    added belongs_to category in advert model

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,6 +1,6 @@
 class Advert < ActiveRecord::Base
   attr_accessible :category_id, :description, :expire, :phone, :price, :title, :user_id
-  belongs_to :user
+  belongs_to :user, :category
   resourcify
   rolify
 end
