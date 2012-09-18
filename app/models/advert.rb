3cdec9875f8d0b00d48702d4db696ec319cbc729
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2012-09-18 21:40:25 +0200

    advert model: added has_many attachments, nested attr, and attachment_attr

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,7 +1,9 @@
 class Advert < ActiveRecord::Base
-  attr_accessible :category_id, :description, :expire, :phone, :price, :title, :user_id
+  attr_accessible :category_id, :description, :expire, :phone, :price, :title, :user_id, :attachments_attributes
   belongs_to :user
   belongs_to :category
+  has_many :attachments, :as => :attachable
+  accepts_nested_attributes_for :attachments
   resourcify
   rolify
 end
