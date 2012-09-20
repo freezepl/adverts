e0be3daf400644ad938f7d2d1b019e018babe96c
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2012-09-21 01:04:59 +0200

    Added truncate and word_wrap for long desc on home#index

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,7 @@
 module HomeHelper
+  def truncate_wrap(text)
+    # text.gsub(/\\n/, ' ')
+    text = truncate(text, :length => 250, :separator => ' ')
+    word_wrap(text, :line_width => 80).gsub(/\n/, '<br>').html_safe
+  end
 end
