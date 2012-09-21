fd920752bc80cf4d12080be552eec025082780c5
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2012-09-21 22:45:20 +0200

    deleted wraping in helper model

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,6 +2,6 @@ module HomeHelper
   def truncate_wrap(text)
     # text.gsub(/\\n/, ' ')
     text = truncate(text, :length => 250, :separator => ' ')
-    word_wrap(text, :line_width => 80).gsub(/\n/, '<br>').html_safe
+    # word_wrap(text, :line_width => 80).gsub(/\n/, '<br>').html_safe
   end
 end
