2c301dd443abc95d7cad081346022c5098dfde0c
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2012-09-21 22:33:30 +0200

    changed compress assets to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,11 +11,11 @@ Adverts::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # Compress JavaScripts and CSS -ZMIANA
+  config.assets.compress = false
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  # Don't fallback to assets pipeline if a precompiled asset is missed - ZMIANA
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
